81aac30e1971f9d687cd3b23b766604b5cbc7b23
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-08-05 22:10:54 +0530

    Add files via upload

---
+++ method_overloading.py
@@ -0,0 +1,8 @@
+# class num:
+#     def sum(self,a=4,c=3,b=3):
+#         print('sum=',a+b+c)
+# n=num()
+# n.sum()         sum= 10
+# n.sum(2,1)      sum= 6
+# n.sum(5,6,7)    sum= 18
+# n.sum(a=2,b=8,c=9)  sum= 19
